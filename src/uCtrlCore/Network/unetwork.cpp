34c3648de14bba1de8766cb6ebd6a20efa84f6e6
Sim Ben <sbenoit10@gmail.com>
2014-03-11 17:14:30 -0400

    Implemention of methods in unetwork.cpp file

--- src/uCtrlCore/Network/unetwork.cpp
+++ src/uCtrlCore/Network/unetwork.cpp
@@ -8,23 +8,32 @@ UNetwork::~UNetwork()
 {
 
 }
-
+/*
 UNetwork::getDeviceInfo(const UDeviceSummary& deviceSummary)
 {
-
+    //reçoit un deviceSummary et retourne un deviceInfo
 }
 
 UNetwork::getAllDevicesSummaries()
 {
 
 }
-
-UNetwork::updateDeviceInfo(UDeviceInfo& deviceInfo)
+*/
+void UNetwork::updateDeviceInfo(UDeviceInfo& deviceInfo)
 {
+    UDeviceInfo* tmpDeviceInfo = new UDeviceInfo(deviceInfo);
+
+    tmpDeviceInfo->minValue = 0.0;
+    tmpDeviceInfo->maxValue = 10.0;
+    tmpDeviceInfo->precision = 1;
+    tmpDeviceInfo->unitLabel = "Temperature";
+    tmpDeviceInfo->type = 0x1011;
+
 
 }
-
+/*
 UNetwork::updateDeviceScenario(int scenarioId, UScenario& scenario)
 {
 
 }
+*/
