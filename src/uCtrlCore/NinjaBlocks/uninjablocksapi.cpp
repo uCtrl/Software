3f1db068cd6b2d31b3329604e3e85999ba2deff1
jpgauthier <jp.gauthier0@gmail.com>
2014-09-16 10:11:23 -0400

    Added base API methods to UNinjaBlockAPI

--- src/uCtrlCore/NinjaBlocks/uninjablocksapi.cpp
+++ src/uCtrlCore/NinjaBlocks/uninjablocksapi.cpp
@@ -4,4 +4,39 @@ UNinjaBlocksAPI::UNinjaBlocksAPI(QNetworkAccessManager* nam, QObject *parent) :
     QObject(parent)
 {
     m_networkAccessManager = nam;
+
+    QObject::connect(m_networkAccessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(parseResponse(QNetworkReply*)));
+}
+
+void UNinjaBlocksAPI::getBlocks()
+{
+    sendRequest("block");
+}
+
+void UNinjaBlocksAPI::getBlock(const QString &blockId)
+{
+}
+
+void UNinjaBlocksAPI::getDevices()
+{
+}
+
+void UNinjaBlocksAPI::getDevice(const QString &deviceId)
+{
+}
+
+void UNinjaBlocksAPI::getRules()
+{
+}
+
+void UNinjaBlocksAPI::sendRequest(const QString &urlString)
+{
+    QUrl url(NinjaBaseUrl + urlString);
+    QNetworkRequest req(url);
+    m_networkAccessManager->get(req);
+}
+
+
+void UNinjaBlocksAPI::parseResponse(QNetworkReply* reply)
+{
 }
--- src/uCtrlCore/NinjaBlocks/uninjablocksapi.h
+++ src/uCtrlCore/NinjaBlocks/uninjablocksapi.h
@@ -4,19 +4,31 @@
 #include <QObject>
 #include <QtNetwork>
 
+const QString NinjaBaseUrl = "https://api.ninja.is/rest/v0/";
+
 class UNinjaBlocksAPI : public QObject
 {
     Q_OBJECT
+
 public:
     explicit UNinjaBlocksAPI(QNetworkAccessManager* nam, QObject *parent = 0);
+    Q_INVOKABLE void getBlocks();
+    Q_INVOKABLE void getBlock(const QString& blockId);
+    Q_INVOKABLE void getDevices();
+    Q_INVOKABLE void getDevice(const QString& deviceId);
+    Q_INVOKABLE void getRules();
 
 signals:
 
 public slots:
 
-private:
-    QNetworkAccessManager* m_networkAccessManager;
+private slots:
+    void parseResponse(QNetworkReply* reply);
 
+private:
+    void sendRequest(const QString &url);
+
+    QNetworkAccessManager* m_networkAccessManager;
 };
 
 #endif // UNINJABLOCKSAPI_H
