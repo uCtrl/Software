cca558de630cb97c65a0391fc797af799f73baa8
frankbew <frank_beauchemin9@hotmail.com>
2014-10-21 15:58:17 -0400

    Last commit hopefully

--- src/uCtrlDesktop/Voice/uturnonoffplugintent.h
+++ src/uCtrlDesktop/Voice/uturnonoffplugintent.h
@@ -3,15 +3,15 @@
 
 #include "uninjaapi.h"
 
-class UTurnOnOffPlugIntent
-{
-    const QString SocketTypeRFCode = "11011010110110101101";
-    const QString SocketOnRFCode = "1";
-    const QString SocketOffRFCode = "0";
-    const QString Socket1RFCode = "010";
-    const QString Socket2RFCode = "100";
-    const QString Socket3RFCode = "110";
+const QString SocketTypeRFCode = "11011010110110101101";
+const QString SocketOnRFCode = "1";
+const QString SocketOffRFCode = "0";
+const QString Socket1RFCode = "010";
+const QString Socket3RFCode = "110";
+const QString Socket2RFCode = "100";
 
+class UTurnOnOffPlugIntent
+{    
     bool m_isTurnOn;
     UNinjaAPI* m_ninjaAPI;
     QString m_deviceId;
